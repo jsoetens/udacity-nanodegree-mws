a41b29059355732d2135c320c2209219ea97b2cc
Soetens, Johan <Johan.Soetens@lidl.be>
2018-05-15 21:08:16 +0200

    chore: project 2 - adding Gulp tasks for copying, building, watching and minifying css

--- mws-project-2/gulpfile.js
+++ mws-project-2/gulpfile.js
@@ -1,13 +1,76 @@
 const gulp = require('gulp');
+const del = require('del');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
 const workboxBuild = require('workbox-build');
+const cleanCSS = require('gulp-clean-css');
 
-// Task: copy
-// Copies files from app to build.
-gulp.task('copy', () =>
+// Task: clean-build
+// Removes all files in build.
+gulp.task('clean-build', () => {
+  return del.sync('build');
+})
+
+// Task: copy-app
+// Copies all app files from app to build.
+gulp.task('copy-app', () =>
   gulp.src([
-    'app/**/*',
-  ]).pipe(gulp.dest('build'))
+    'app/*.html',
+    'app/css/**/*.css',
+    'app/js/**/*.js',
+    'app/img/**',
+    'app/favicon.ico',
+    'app/manifest.json'
+  ], {base: 'app/.'})
+  .pipe(gulp.dest('build'))
 );
+
+// Task: copy-html
+gulp.task('copy-html', () =>
+  gulp.src('app/*.html')
+  .pipe(gulp.dest('build'))
+);
+
+// Task: copy-css
+gulp.task('copy-css', () =>
+  gulp.src('app/css/**/*.css')
+  .pipe(gulp.dest('build/css'))
+);
+
+// Task: copy-js
+gulp.task('copy-js', () =>
+  gulp.src('app/js/**/*.js')
+  .pipe(gulp.dest('build/js'))
+);
+
+// Task: copy-images
+gulp.task('copy-images', () =>
+  gulp.src('app/img/**')
+  .pipe(gulp.dest('build/img'))
+);
+
+// Task: copy-favicon
+gulp.task('copy-favicon', () =>
+  gulp.src('app/favicon.ico')
+  .pipe(gulp.dest('build'))
+);
+
+// Task: copy-manifest
+gulp.task('copy-manifest', () =>
+  gulp.src('app/manifest.json')
+  .pipe(gulp.dest('build'))
+);
+
+// Task: minify-css
+// Minify CSS using clean-css.
+gulp.task('minify-css', () => {
+  return gulp.src('app/css/**/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+  .pipe(gulp.dest('build/css'));
+});
 
 // Task: service-worker
 gulp.task('service-worker', () => {
@@ -17,6 +80,7 @@ gulp.task('service-worker', () => {
     globDirectory: 'build',
     globPatterns: [
       "**/*.{html,css,js}",
+      "favicon.ico",
       "manifest.json",
       "img/touch/*.png"
     ],
@@ -30,21 +94,40 @@ gulp.task('service-worker', () => {
   });
 });
 
-// Task: default
-gulp.task('default', ['copy', 'service-worker']);
-
-// Task: watch
-// this task watches our "app" files & rebuilds whenever they change
-gulp.task('watch', function() {
-  gulp.watch('app/**/*', ['copy', 'service-worker']);
+// Task: browser-sync
+// Browsersync static server.
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./app"
+    }
+  });
 });
 
-// Gulp 4
-// Task: default tasks (Gulp 4)
-// gulp.task('default', gulp.series('copy', 'service-worker'));
+// Task: default
+gulp.task('default', cb => {
+  runSequence(
+    'clean-build',
+    'copy-app',
+    'service-worker', cb);
+});
 
-// Task: watch (Gulp 4)
-// function watchApp() {
-//     return gulp.watch('app/**/*', gulp.series('copy', 'service-worker'));
-// }
-// gulp.task('watch', watchApp);
+// Task: watch
+// This task watches our "app" files & rebuilds whenever they change.
+gulp.task('watch', () => {
+  gulp.watch('app/*.html', ['copy-html']);
+  gulp.watch('app/css/**/*.css', ['copy-css']);
+  gulp.watch('app/js/**/*.js', ['copy-js']);
+  gulp.watch('app/img/**', ['copy-images']);
+  gulp.watch('app/manifest.json', ['copy-manifest']);
+});
+
+// Task: build
+// Build the production app.
+gulp.task('build', cb => {
+  runSequence(
+    'clean-build',
+    ['copy-html', 'copy-js', 'copy-images', 'copy-favicon', 'copy-manifest'],
+    'minify-css', 'service-worker',
+    cb);
+});
